29a0cdb607a57140855cfafd3225319f4ec70611
PPnaruto <prtaik.35.patil@gmail.com>
2022-10-12 04:26:22 +0530

    Linked to Product Page

--- Shop All/shop.js
+++ Shop All/shop.js
@@ -83,6 +83,7 @@ var productsdata = [
 displaydata(productsdata);
 
 function displaydata(data){
+    document.querySelector("#productbox").innerHTML = "";
     data.map(function(ele){
         var div = document.createElement("div");
 
@@ -105,15 +106,60 @@ function displaydata(data){
             // console.log(ele);
             getdata(ele);
         });
+        // div.setAttribute("href","product.html");
         document.querySelector("#productbox").append(div);
         // localStorage.setItem("products",JSON.stringify(data));
     })
 }
 
-var storedata = JSON.parse(localStorage.getItem("product")) || [];
+var storedata = [];
 function getdata(ele){
     storedata.push(ele);
     localStorage.setItem("product",JSON.stringify(storedata));
-}
+    window.location.href= "file:///C:/Users/Pratik%20Patil/Documents/VS%20Code/Project_Lovoda/Project_Lovoda/Product%20page/product.html";
+} 
+
+function SortData(){
+    var alpha = document.getElementById("sortby").value;
+    if(alpha === "normal"){
+        // productsdata.sort(function(a,b){
+        //     if(a.name>b.name) return 1;
+        //     if(a.name<b.name) return -1;
+        //     return 0;
+        // });
+        displaydata(productsdata);
+        };
+    if(alpha === "A-Z"){
+        productsdata.sort(function(a,b){
+            if(a.name>b.name) return 1;
+            if(a.name<b.name) return -1;
+            return 0;
+        });
+        displaydata(productsdata);
+        };
+       
+    if(alpha === "Z-A"){
+        productsdata.sort(function(a,b){
+            if(a.name>b.name) return -1;
+            if(a.name<b.name) return 1;
+            return 0;
+            });
+        displaydata(productsdata);
+        };
+    if(alpha === "lth"){
+        productsdata.sort(function(a,b){
+            return a.price-b.price;
+            });
+            displaydata(productsdata);
+    }
+    if(alpha === "htl"){
+        productsdata.sort(function(a,b){
+            return b.price-a.price;
+            });
+            displaydata(productsdata);
+    }
+}     
+        
+    
 
 //div => div=>img,name,price
