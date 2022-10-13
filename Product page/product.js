d0c0d0cc73477d746ece7f3960d37ec9c2f463c5
PPnaruto <prtaik.35.patil@gmail.com>
2022-10-13 21:09:34 +0530

    Completed Js Part of productpage

--- Product page/product.js
+++ Product page/product.js
@@ -71,20 +71,43 @@ minus.addEventListener("click",()=>{
     num.innerText = qty;
     product[0].qty = qty;
 })
-
+var cartdata = JSON.parse(localStorage.getItem("cartdata")) || [];
 document.querySelector(".atc").addEventListener("click",()=>{
     document.getElementById("content").style.display = "block";
+    var image = document.createElement("img");
+    image.setAttribute("src",product[0].image);
+    image.setAttribute("id","adjustimage");
+    var name = document.createElement("h4");
+    name.innerText = product[0].name;
+    var price = document.createElement("h5");
+    price.innerText = "Price:$" + product[0].price;
+    document.getElementById("imginfo").append(image);
+    document.getElementById("nameinfo").append(name,price);
+
+    var data = cartdata.filter(function(ele){
+        return ele.name === product[0].name;
+    });
+    if(data.length === 0){
+        cartdata.push(product[0]);
+        product[0].qty = qty;
+    }else{
+        product[0].qty = product[0].qty;
+        console.log(product[0].qty);
+    }
+    localStorage.setItem("cartdata",JSON.stringify(cartdata));
 })
 document.querySelector("#hidebox").addEventListener("click",()=>{
     document.getElementById("content").style.display = "none";
+    window.location.href = "file:///C:/Users/Pratik%20Patil/Documents/VS%20Code/Project_Lovoda/Project_Lovoda/shop.html";
 })
 
-var cartdata = JSON.parse(localStorage.getItem("cartdata")) || [];
+// var cartdata = JSON.parse(localStorage.getItem("cartdata")) || [];
+
 document.querySelector("#mycart").addEventListener("click",function(){
-  cartdata.push(product[0]);
-  console.log(cartdata);
-  localStorage.setItem("cartdata",JSON.stringify(cartdata));
-})
+    // window.location.href = "dummycart.html";
+    alert("Please coonect cart page now");
+    });
+
 
 //div => img1,img2,img3
 //div => text all
