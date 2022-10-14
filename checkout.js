b3a1dd93ebaede3cc9b4af88cf75a8170ef23f33
adityarajbenn <adityarajbenn@gmail.com>
2022-10-14 21:15:22 +0530

    checkout html/Js fixed

--- checkout.js
+++ checkout.js
@@ -1,9 +1,13 @@
 var productdata = JSON.parse(localStorage.getItem("cartdata"));
 // console.log(productdata);
-displaydata(productdata);
 
+// if(productdata.length === 0){
+
+// }
+displaydata(productdata);
+displayprice(productdata);
 function displaydata(data){
-    document.getElementById("data12") = "";
+    document.getElementById("data12").innerHTML = "";
     data.map(function(ele){
         var div = document.createElement("div");
         div.setAttribute("id","cartProducts");
@@ -27,24 +31,30 @@ function displaydata(data){
         var totaldiv = document.createElement("div");
         totaldiv.setAttribute("id","cartJSPart");
         var btndiv = document.createElement("div");
-        btndiv.setAttribute("id","qtybox");
+        btndiv.setAttribute("class","qtybox");
         var span1 = document.createElement("span");
         span1.innerText = "-";
         span1.setAttribute("id","minus");
+        span1.addEventListener("click",function(){
+            subtractqty(ele)
+        });
         var span2 = document.createElement("span");
         span2.innerText = ele.qty;
-        span1.setAttribute("id","num");
+        span2.setAttribute("id","num");
         var span3 = document.createElement("span");
         span3.innerText = "+";
         span3.setAttribute("id","plus");
         span3.addEventListener("click",function(){
-            addqty(ele);
-        })
+            addqty(ele)
+        });
         btndiv.append(span1, span2, span3);
         
         var btn = document.createElement("button");
         btn.innerHTML = '<i class="fa-solid fa-trash"></i>';
         btn.setAttribute("id","cartProducts-deleteBtn");
+        btn.addEventListener("click",function(){
+            deletedata(ele);
+        });
         
         var totalprice = document.createElement("div");
         totalprice.setAttribute("id","cartProducts-totalPrice");
@@ -60,27 +70,37 @@ function displaydata(data){
 }
 
 
+
+
 function addqty(ele){
     ele.qty++;
-    
-    displaydata()
+    document.getElementById("num").innerText = ele.qty;
+    console.log(ele.qty);
+    localStorage.setItem("cartdata",JSON.stringify(productdata));
+    displaydata(productdata);
+    displayprice(productdata);
 }
-var plus = document.querySelector("#plus");
-var num = document.querySelector("#num");
-var minus = document.querySelector("#minus");
+function subtractqty(ele){
+    ele.qty--;
+    document.getElementById("num").innerText = ele.qty;
+    console.log(ele.qty);
+    localStorage.setItem("cartdata",JSON.stringify(productdata));
+    displaydata(productdata);
+    displayprice(productdata);
+}
+function displayprice(data){
+    var overalltotal = data.reduce(function(acc,ele){
+        return acc + (ele.qty*(parseInt(ele.price)));
+    },0)
+    document.getElementById("Overallprice").innerText = "$" + overalltotal;
+}
 
-plus.addEventListener("click",()=>{
-    qty++;
-    num.innerText = qty;
-    product[0].qty = qty;
-    displaydata()
-})
-
-minus.addEventListener("click",()=>{
-    qty--;
-    num.innerText = qty;
-    product[0].qty = qty;
-})
+function deletedata(ele){
+    productdata.splice(ele,1);
+    localStorage.setItem("cartdata",JSON.stringify(productdata));
+    displaydata(productdata);
+    displayprice(productdata);
+}
 
 
 
