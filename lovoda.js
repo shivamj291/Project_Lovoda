f7febb6c68425c115d258a9f2ff02585b581d537
PPnaruto <prtaik.35.patil@gmail.com>
2022-10-14 23:13:07 +0530

    add anchoring from home to payment page

--- lovoda.js
+++ lovoda.js
@@ -76,9 +76,9 @@ var storedata = [];
 function getdata(ele){
     storedata.push(ele);
     localStorage.setItem("product",JSON.stringify(storedata));
-    window.location.href= "file:///C:/Users/Pratik%20Patil/Documents/VS%20Code/Project_Lovoda/Project_Lovoda/Product%20page/product.html";
+    window.location.href= "Product page/product.html";
 } 
-
+document.getElementById
 // function SortData(){
 //     var alpha = document.getElementById("sortby").value;
 //     if(alpha === "normal"){
