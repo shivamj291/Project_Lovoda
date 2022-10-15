187c3a9cf322acf5cb45bbf059e315c0436d9892
adityarajbenn <adityarajbenn@gmail.com>
2022-10-15 07:22:25 +0530

    added search bar

--- lovoda.js
+++ lovoda.js
@@ -1,3 +1,15 @@
+// const search = document.querySelector('.search')
+// const btn = document.querySelector('.btn')
+// const input = document.querySelector('.input')
+document.getElementById("btn").addEventListener("click",()=>{
+    document.getElementById("searchParent").style.display = "block";
+    document.body.style.opacity= ".5";
+    
+})
+document.getElementById("searchClose").addEventListener("click",()=>{
+    document.getElementById("searchParent").style.display = "none";
+    document.body.style.opacity= "1";
+})
 var productsdata = [
     {image : "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101104-2_1.jpg?v=1659450395",
      hoverimage :"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101104-3_1_823x.jpg?v=1659450395",
@@ -76,5 +88,46 @@ var storedata = [];
 function getdata(ele){
     storedata.push(ele);
     localStorage.setItem("product",JSON.stringify(storedata));
-    window.location.href= "file:///C:/Users/Pratik%20Patil/Documents/VS%20Code/Project_Lovoda/Project_Lovoda/Product%20page/product.html";
+    window.location.href= "Product page/product.html";
 } 
+// document.getElementById
+// function SortData(){
+//     var alpha = document.getElementById("sortby").value;
+//     if(alpha === "normal"){
+//         // productsdata.sort(function(a,b){
+//         //     if(a.name>b.name) return 1;
+//         //     if(a.name<b.name) return -1;
+//         //     return 0;
+//         // });
+//         displaydata(productsdata);
+//         };
+//     if(alpha === "A-Z"){
+//         productsdata.sort(function(a,b){
+//             if(a.name>b.name) return 1;
+//             if(a.name<b.name) return -1;
+//             return 0;
+//         });
+//         displaydata(productsdata);
+//         };
+       
+//     if(alpha === "Z-A"){
+//         productsdata.sort(function(a,b){
+//             if(a.name>b.name) return -1;
+//             if(a.name<b.name) return 1;
+//             return 0;
+//             });
+//         displaydata(productsdata);
+//         };
+//     if(alpha === "lth"){
+//         productsdata.sort(function(a,b){
+//             return a.price-b.price;
+//             });
+//             displaydata(productsdata);
+//     }
+//     if(alpha === "htl"){
+//         productsdata.sort(function(a,b){
+//             return b.price-a.price;
+//             });
+//             displaydata(productsdata);
+//     }
+// }    
